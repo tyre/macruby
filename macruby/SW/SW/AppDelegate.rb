e28a259462bcdbf7e97509cefb8f35c1af00eb67
Chris Maddox <tyre77@gmail.com>
2012-04-09 15:34:04 -0400

    SW ->  a random callout generator for professors¡

--- macruby/SW/SW/AppDelegate.rb
+++ macruby/SW/SW/AppDelegate.rb
@@ -3,40 +3,66 @@
 #  SW
 #
 #  Created by Chris Maddox on 4/8/12.
-#  Copyright 2012 __MyCompanyName__. All rights reserved.
+#  Copyright 2012 Chris Maddox. All rights reserved.
 #
 
 class AppDelegate
-  attr_accessor :window, :addNameField, :statsWindow, :table, :comboBox, :currentVictim, :statsVictim, :roster, :classWindow
+  attr_accessor :window, :addNameField, :currentVictim, :roster, :table
   def applicationDidFinishLaunching(a_notification)
-    self.comboBox.setUsesDataSource = true
-    self.comboBox.setDataSource = self.roster
-    self.classWindow.setReleasedWhenClosed = false
-    self.statsWindow.setReleasedWhenClosed = false
+    @table.setDataSource(self.roster)
   end
 
   def roster
-    @roster ||= []
+    @roster ||= TableViewDataSource.new
   end
   
   def random(sender)
     if @roster.any?
-      @currentVictim = @roster.shuffle.first
+      victim = self.roster.random
+      self.currentVictim.setStringValue(victim)
     else
-      @currentVictim = "Add people to the roster first!"
+      self.currentVictim.setStringValue("Add people to the roster first!")
     end
   end
   
-  def showClassStats(sender)
-    self.classWindow.makeKeyAndOrderFront
-  end
-  
-  def showStudentStats(sender)
-    self.statsVictim = self.roster[comboBox.indexOfSelectedItem]
-    self.statsWindow.makeKeyAndOrderFront
-  end
-  
   def addStudentToRoster(sender)
-    self.roster << addNameField.stringValue
+    self.roster.tableView(@table,
+                          setObjectValue: addNameField.stringValue,
+                          forTableColumn: 0,
+                          row: roster.numberOfRowsInTableView(@table))
+  puts self.roster.inspect
+  self.table.reloadData
+  end
+end
+
+class TableViewDataSource
+  attr_accessor :data
+  
+  def initialize
+    self.data = [Array.new]
+  end
+
+  def any?
+    data[0].any?
+  end
+  
+  def tableView(table_view, setObjectValue: value, forTableColumn: column, row: row)
+    self.data[table_view.columnWithIdentifier(column)][row] = value
+    self.data[0].uniq!
+  end
+  
+  def tableView(table_view, objectValueForTableColumn: column, row: row)
+    self.data[table_view.columnWithIdentifier(column)][row]
+  end
+  
+  def numberOfRowsInTableView(view)
+    self.data[0].size
+  end
+  
+  def random
+    column = self.data[0]
+    r_row = rand(column.size)
+    puts "ROW: #{r_row}"
+    column[r_row]
   end
 end
